f626b824a48fa1e0e42005cd88d7c7f121e89557
Richard Okonicha <Richard Okonicha>
2020-07-01 18:19:16 +0100

    Header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>
+            Balkan Style <span>[ Portfolio Gallery ]</span>
+          </h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
