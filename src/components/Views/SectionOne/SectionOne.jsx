1bb2e0a03f365dfd8dbc8760d64731a281845041
Richard Okonicha <richardokonicha@gmail.com>
2020-08-02 23:48:12 +0100

    projects img

--- src/components/Views/SectionOne/SectionOne.jsx
+++ src/components/Views/SectionOne/SectionOne.jsx
@@ -8,7 +8,18 @@ export default function SectionOne() {
         <div className="SectionOne__grid">
           <div className="SectionOne__grid__item">
             <article className="pantone-card pantone-card--french-gray">
-              <h1 className="pantone-card__name">this is the beginning</h1>
+              {/*  <img
+                className="SectionOne__ImageIss"
+                src="https://res.cloudinary.com/konichar/image/upload/v1596406145/ISS_yxxr05.png"
+                alt=""
+                srcset=""
+              /> */}
+              <img
+                className="SectionOne__ImageIss"
+                src="https://res.cloudinary.com/konichar/image/upload/v1596406144/openhisa_zxgisc.png"
+                alt=""
+                srcset=""
+              />
             </article>
           </div>
           <div className="SectionOne__grid__item">
@@ -26,43 +37,83 @@ export default function SectionOne() {
           </div>
           <div className="SectionOne__grid__item">
             <article className="pantone-card pantone-card--mischka">
-              <h1 className="pantone-card__name">this is the beginning</h1>
+              <img
+                className="SectionOne__ImageIss"
+                src="https://res.cloudinary.com/konichar/image/upload/v1596406145/ISS_yxxr05.png"
+                alt=""
+                srcset=""
+              />
             </article>
           </div>
           <div className="SectionOne__grid__item">
             <article className="pantone-card pantone-card--givry">
-              <h1 className="pantone-card__name">this is the beginning</h1>
+              <img
+                className="SectionOne__ImageIss"
+                src="https://res.cloudinary.com/konichar/image/upload/v1596406145/ISS_yxxr05.png"
+                alt=""
+                srcset=""
+              />
             </article>
           </div>
           <div className="SectionOne__grid__item">
             <article className="pantone-card pantone-card--bon-jour">
-              <h1 className="pantone-card__name">this is the beginning</h1>
+              <img
+                className="SectionOne__ImageIss"
+                src="https://res.cloudinary.com/konichar/image/upload/v1596406145/ISS_yxxr05.png"
+                alt=""
+                srcset=""
+              />
             </article>
           </div>
 
           <div className="SectionOne__grid__item">
             <article className="pantone-card pantone-card--jagged-ice">
-              <h1 className="pantone-card__name">this is the beginning</h1>
+              <img
+                className="SectionOne__ImageIss"
+                src="https://res.cloudinary.com/konichar/image/upload/v1596406145/ISS_yxxr05.png"
+                alt=""
+                srcset=""
+              />
             </article>
           </div>
           <div className="SectionOne__grid__item">
             <article className="pantone-card pantone-card--shadow-green">
-              <h1 className="pantone-card__name">this is the beginning</h1>
+              <img
+                className="SectionOne__ImageIss"
+                src="https://res.cloudinary.com/konichar/image/upload/v1596406145/ISS_yxxr05.png"
+                alt=""
+                srcset=""
+              />
             </article>
           </div>
           {/* <div className="SectionOne__grid__item">
             <article className="pantone-card pantone-card--conch">
-              <h1 className="pantone-card__name">this is the beginning</h1>
+               <img
+                className="SectionOne__ImageIss"
+                src="https://res.cloudinary.com/konichar/image/upload/v1596406145/ISS_yxxr05.png"
+                alt=""
+                srcset=""
+              />
             </article>
           </div>
           <div className="SectionOne__grid__item">
             <article className="pantone-card pantone-card--gray-nurse">
-              <h1 className="pantone-card__name">this is the beginning</h1>
+               <img
+                className="SectionOne__ImageIss"
+                src="https://res.cloudinary.com/konichar/image/upload/v1596406145/ISS_yxxr05.png"
+                alt=""
+                srcset=""
+              />
             </article>
           </div>
           <div className="SectionOne__grid__item">
             <article className="pantone-card pantone-card--pampas">
-              <h1 className="pantone-card__name">this is the beginning</h1>
+               <img
+                className="SectionOne__ImageIss"
+                src="https://res.cloudinary.com/konichar/image/upload/v1596406145/ISS_yxxr05.png"
+                alt=""
+                srcset=""
+              />
             </article>
           </div> */}
         </div>
